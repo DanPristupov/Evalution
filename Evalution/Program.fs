62960bfb0cb294bc1cf0e807e83cdd8c28a39f4a
Dan <danil@danil.cz>
2014-08-17 10:42:30 +0200

    Made code more functional :). Introduces getToken function

--- Evalution/Program.fs
+++ Evalution/Program.fs
@@ -10,43 +10,47 @@
         | Operator of char
 
     let readFormula input = 
+        let length = String.length(input)
 
         let isPartOfNumeric char =
             match char with
             | x when x = decimalSeparator -> true
             | x when x > '0' && x < '9' -> true
             | _ -> false
-        
-        let mutable tokens = []
-        let mutable i = 0
-        while i < String.length(input) do
-            let mutable buffer = input.[i].ToString()
 
+        let getToken start = 
+            let mutable i = start
+            let mutable buffer = ""
             if isPartOfNumeric input.[i] then
-                while i < String.length(input) && (isPartOfNumeric input.[i]) do
+                while i< String.length(input) && (isPartOfNumeric input.[i]) do
                     buffer <- buffer + input.[i].ToString()
                     i <- i+1
-                i <- i-1
 
                 let (succ, intValue) = Int32.TryParse buffer
                 if succ then
-                    tokens <- (Integer intValue)::tokens
+                    ((Integer intValue), i)
                 else
                     let (succ, doubleValue) = Double.TryParse(buffer, NumberStyles.Float ||| NumberStyles.AllowThousands, CultureInfo.CurrentCulture)
                     if (succ) then
-                        tokens <- (Double doubleValue)::tokens
-                i <- i+1
+                        ((Double doubleValue), i)
+                    else
+                        failwith "fail"
             else
                 match input.[i] with
-                | ('+' | '-' | '*' | '/' | '(' | ')') -> tokens <- (Operator input.[i])::tokens
-                | _ -> tokens <- tokens
-                i <- i+1
+                | ('+' | '-' | '*' | '/' | '(' | ')') -> ((Operator input.[i]), i+1)
+                | _ -> failwith "fail"
 
+        let mutable tokens = []
+        let mutable i = 0
+        while i < String.length(input) do
+            let (token, nextPos) = getToken i
+            tokens <- (token::tokens)
+            i <- nextPos
         tokens
  
     [<EntryPoint>]
     let main argv = 
-        let result = readFormula("(42+8)* 2")
+        let result = readFormula("(42+8)*2")
         result |> Seq.iter ( printfn "%A")
 
         Console.ReadLine()
