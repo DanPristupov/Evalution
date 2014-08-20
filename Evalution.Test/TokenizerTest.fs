a5974243897c0f5b84e7d6fca271dda13e1738fa
Dan <danil@danil.cz>
2014-08-20 19:00:10 +0200

    Adds draft unit tests for Tokenizer

---
+++ Evalution.Test/TokenizerTest.fs
@@ -0,0 +1,61 @@
+﻿namespace UnitTestProject1
+
+open System
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open Evalution
+
+[<TestClass>]
+type TokenizerTest() = 
+    let tokenizer = new Tokenizer()
+
+    [<TestMethod>]
+    member x.TestTokenReader1 ()=
+        let result = tokenizer.Read "42+31"
+        
+        let expectedResult = [
+            Integer 42;
+            Operator '+';
+            Integer 31;
+        ]
+        Assert.AreEqual(expectedResult, result)
+
+    [<TestMethod>]
+    member x.TestTokenReader2 ()=
+        let result = tokenizer.Read "(42+31)"
+        
+        let expectedResult = [
+            Operator '(';
+            Integer 42;
+            Operator '+';
+            Integer 31;
+            Operator ')';
+        ]
+        Assert.AreEqual(expectedResult, result)
+
+    [<TestMethod>]
+    member x.TestTokenReader3 ()=
+        let result = tokenizer.Read "(42+31.0)"
+        
+        let expectedResult = [
+            Operator '(';
+            Integer 42;
+            Operator '+';
+            Double 31.0;
+            Operator ')';
+        ]
+        Assert.AreEqual(expectedResult, result)
+
+    [<TestMethod>]
+    member x.TestTokenReader4 ()=
+        let result = tokenizer.Read "(42+ 8) *2"
+        
+        let expectedResult = [
+            Operator '(';
+            Integer 42;
+            Operator '+';
+            Integer 8;
+            Operator ')';
+            Operator '*';
+            Integer 2;
+        ]
+        Assert.AreEqual(expectedResult, result)
