8bc495b7d56993fbf9b068e984d2e000f57c39b0
Dan <danil@danil.cz>
2014-09-21 15:59:04 +0200

    Adds unit test with double type

--- Evalution.CSharpTest/EvaluatorTest.cs
+++ Evalution.CSharpTest/EvaluatorTest.cs
@@ -10,12 +10,24 @@ namespace Evalution.CSharpTest
         public void GeneralTest_Integer()
         {
             var evaluator = new Evaluator();
-            var target = evaluator.BuildObject<ClassWithDependency>();
+            var target = evaluator.BuildObject<ClassInt32>();
 
             target.Value1 = 4;
             Assert.AreEqual(6, target.ValueWithExpression); // "2+2*2"
             Assert.AreEqual(8, target.DependentValue1);     // "Value1*2"
             Assert.AreEqual(16, target.DependentValue2);    // "DependentValue1*2"
+        }
+
+        [TestMethod]
+        public void GeneralTest_Double()
+        {
+            var evaluator = new Evaluator();
+            var target = evaluator.BuildObject<ClassDouble>();
+
+            target.Value1 = 1.5;
+            Assert.AreEqual(7.0, target.ValueWithExpression);   // "2.0 + 2.0 * 2.5"
+            Assert.AreEqual(3.0, target.DependentValue1);       // "Value1 * 2.0"
+            Assert.AreEqual(6.0, target.DependentValue2);       // "DependentValue1 * 2.0"
         }
     }
 }
--- Evalution.CSharpTest/TestClass.cs
+++ Evalution.CSharpTest/TestClass.cs
@@ -1,16 +1,30 @@
 ﻿namespace Evalution.CSharpTest
 {
-    public class ClassWithDependency
+    public class ClassInt32
     {
         public int Value1 { get; set; }
 
         [Expression("2+2*2")]
         public virtual int ValueWithExpression { get; set; }
 
-        [Expression("Value1*2")] 
+        [Expression("Value1*2")]
         public virtual int DependentValue1 { get; set; }
 
         [Expression("DependentValue1*2")]
         public virtual int DependentValue2 { get; set; }
+    }
+
+    public class ClassDouble
+    {
+        public double Value1 { get; set; }
+
+        [Expression("2.0+2.0*2.5")]
+        public virtual double ValueWithExpression { get; set; }
+
+        [Expression("Value1*2.0")]
+        public virtual double DependentValue1 { get; set; }
+
+        [Expression("DependentValue1*2.0")]
+        public virtual double DependentValue2 { get; set; }
     }
 }
