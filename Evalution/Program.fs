ed35541f5409734f83ec3ac933a45211b3f38bad
Dan <danil@danil.cz>
2014-10-01 22:38:16 +0200

    Adds binary operations

--- Evalution/Program.fs
+++ Evalution/Program.fs
@@ -14,11 +14,23 @@
         let int32Literal = configurator.CreateTerminal(@"\d+", fun x -> box (Ast.Int32Literal (int x) ) )
         let doubleLiteral = configurator.CreateTerminal(@"\d+\.\d+", fun x -> box (Ast.DoubleLiteral (float x) ) )
         let plus = configurator.CreateTerminal(@"\+")
+        let minus = configurator.CreateTerminal(@"-")
+        let asterisk = configurator.CreateTerminal(@"\*")
+        let forwardSlash = configurator.CreateTerminal(@"/")
 
         configurator.LeftAssociative(plus)
+        configurator.LeftAssociative(minus)
+        configurator.LeftAssociative(asterisk)
+        configurator.LeftAssociative(forwardSlash)
 
         expressionSpec.AddProduction(expressionSpec, plus, expressionSpec)
             .SetReduceFunction(fun x -> box (Ast.BinaryExpression(x.[0] :?> Ast.Expression, Ast.Add, x.[2] :?>Ast.Expression)))
+        expressionSpec.AddProduction(expressionSpec, minus, expressionSpec)
+            .SetReduceFunction(fun x -> box (Ast.BinaryExpression(x.[0] :?> Ast.Expression, Ast.Subtract, x.[2] :?>Ast.Expression)))
+        expressionSpec.AddProduction(expressionSpec, asterisk, expressionSpec)
+            .SetReduceFunction(fun x -> box (Ast.BinaryExpression(x.[0] :?> Ast.Expression, Ast.Multiply, x.[2] :?>Ast.Expression)))
+        expressionSpec.AddProduction(expressionSpec, forwardSlash, expressionSpec)
+            .SetReduceFunction(fun x -> box (Ast.BinaryExpression(x.[0] :?> Ast.Expression, Ast.Divide, x.[2] :?>Ast.Expression)))
 
         expressionSpec.AddProduction(int32Literal)
             .SetReduceFunction(fun x -> box (Ast.LiteralExpression(x.[0]:?>Ast.Literal ) ))
@@ -29,7 +41,7 @@
             .SetReduceToFirst()
 
         let parser = configurator.CreateParser()
-        let aa = parser.Parse("23+20.2")
+        let aa = parser.Parse("23+20.2*20")
 
 
 
