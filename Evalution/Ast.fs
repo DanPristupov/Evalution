34471c5f956118c4c4e92c8efc96b3619ae5374d
Dan <danil@danil.cz>
2014-10-01 21:59:18 +0200

    Adds Int32/Double draft parser

---
+++ Evalution/Ast.fs
@@ -0,0 +1,16 @@
+﻿module Evalution.Ast
+type Program = Expression
+
+and Expression =
+    | LiteralExpression of Literal
+    | BinaryExpression of Expression * BinaryOperator * Expression
+and Literal =
+    | BoolLiteral of bool
+    | Int32Literal of int
+    | DoubleLiteral of float
+
+and BinaryOperator =
+    | Add
+    | Subtract
+    | Multiply
+    | Divide
--- Evalution/Program.fs
+++ Evalution/Program.fs
@@ -2,8 +2,52 @@
     open System
     open Evalution
 
+    open Piglet.Parser
+
     [<EntryPoint>]
     let main argv = 
+        
+        let configurator = ParserFactory.Configure<obj>()
+
+        let expressionSpec = configurator.CreateNonTerminal()
+        let literalSpec = configurator.CreateNonTerminal()
+        let binaryOperatorSpec = configurator.CreateNonTerminal()
+
+        let int32Literal = configurator.CreateTerminal(@"\d+", fun x -> box (Ast.Int32Literal (int x) ) )
+        let doubleLiteral = configurator.CreateTerminal(@"\d+\.\d+", fun x -> box (Ast.DoubleLiteral (float x) ) )
+        let plus = configurator.CreateTerminal(@"\+")
+
+        configurator.RightAssociative(plus)
+
+        expressionSpec.AddProduction(literalSpec)
+            .SetReduceFunction(fun x -> x.[0])
+//        expressionSpec.AddProduction(binaryOperatorSpec)
+//            .SetReduceFunction(fun x -> box (Ast.BinaryExpression(x.[0] :?> Ast.Expression, Ast.Add, x.[2] :?>Ast.Expression)))
+
+        literalSpec.AddProduction(int32Literal)
+            .SetReduceFunction(fun x -> box (Ast.LiteralExpression(x.[0]:?>Ast.Literal ) ))
+        literalSpec.AddProduction(doubleLiteral)
+            .SetReduceFunction(fun x -> box (Ast.LiteralExpression(x.[0]:?>Ast.Literal ) ))
+//        expressionSpec.AddProduction(expressionSpec, plus, expressionSpec)
+//            .SetReduceFunction(fun x -> box (Ast.BinaryExpression(x.[0] :?> Ast.Expression, Ast.Add, x.[2] :?>Ast.Expression)))
+
+        //expressionSpec.AddProduction(doubleLiteral)
+        //expressionSpec.AddProduction(int32Literal)
+        let parser = configurator.CreateParser()
+        let aa = parser.Parse("23.0")
+//        let aa = parser.Parse("23.3 + 22.3")
+
+
+
+
+
+
+
+
+
+
+
+
         let tokenizer = new Tokenizer()
         let tokens = tokenizer.Read("(42+8) *2")
         tokens |> Seq.iter ( printfn "%A")
