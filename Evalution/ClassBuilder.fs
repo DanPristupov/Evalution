1668b9fa0711b2f701620866d6d8e1f0111ecbc0
Dan <danil@danil.cz>
2014-10-05 18:30:56 +0200

    Refactors ClassBuilder

--- Evalution/ClassBuilder.fs
+++ Evalution/ClassBuilder.fs
@@ -13,56 +13,58 @@ type public ClassBuilder(targetType:Type) =
     let typeProperties = lazy (targetType.GetProperties())
 
     let createType (objType: Type):Type =
+
         let createTypeBuilder (objType:Type) =
-            let typeSignature = "EV" + objType.Name
-            let assemblyName = new AssemblyName(typeSignature) // may be I should use assembly of the objType?
+            // todo: should assemblyName be the same for all classes?
+            let assemblyName = new AssemblyName("EV_" + objType.Name) // may be I should use assembly of the objType?
             let assemblyBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.Run)
             let moduleBuilder = assemblyBuilder.DefineDynamicModule("EvalutionModule");
-            let typeBuilder = moduleBuilder.DefineType(typeSignature,
-                                    TypeAttributes.Public |||
-                                    TypeAttributes.Class |||
-                                    TypeAttributes.AutoClass |||
-                                    TypeAttributes.AnsiClass |||
-                                    TypeAttributes.BeforeFieldInit |||
-                                    TypeAttributes.AutoLayout,
-                                    null)
+            let typeBuilder = moduleBuilder.DefineType("EV" + objType.Name,
+                TypeAttributes.Public |||
+                TypeAttributes.Class |||
+                TypeAttributes.AutoClass |||
+                TypeAttributes.AnsiClass |||
+                TypeAttributes.BeforeFieldInit |||
+                TypeAttributes.AutoLayout,
+                null)
+            typeBuilder.DefineDefaultConstructor(MethodAttributes.Public |||
+                                                    MethodAttributes.SpecialName |||
+                                                    MethodAttributes.RTSpecialName) |> ignore
+            typeBuilder.SetParent(objType)
             typeBuilder
 
         let typeBuilder = createTypeBuilder objType
-        typeBuilder.DefineDefaultConstructor(MethodAttributes.Public |||
-                                                MethodAttributes.SpecialName |||
-                                                MethodAttributes.RTSpecialName) |> ignore
-        typeBuilder.SetParent(objType)
+        let targetTypeProperties = objType.GetProperties()
 
-        let createGetPropertyMethodBuilder(propertyName, propertyType:Type, expression, allProperties):MethodBuilder =
-            let emitter = Emit.BuildMethod(propertyType,Array.empty,typeBuilder, "get_"+propertyName, MethodAttributes.Public ||| MethodAttributes.SpecialName ||| MethodAttributes.HideBySig ||| MethodAttributes.Virtual,CallingConventions.Standard ||| CallingConventions.HasThis)
+        let createGetPropertyMethodBuilder(propertyName, propertyType:Type, expression):MethodBuilder =
+            let emitter = Emit.BuildMethod(propertyType, Array.empty, typeBuilder, "get_"+propertyName,
+                MethodAttributes.Public ||| MethodAttributes.SpecialName ||| MethodAttributes.HideBySig ||| MethodAttributes.Virtual,
+                CallingConventions.Standard ||| CallingConventions.HasThis)
+            
+            let rec generateMulticallBody (multicall: Ast.MultiCall, thisType: Type) =
+                let createPropertyCall (typeProperties : PropertyInfo[], propertyName) =
+                    let targetProperty = typeProperties |> Seq.find(fun x -> x.Name = propertyName)
+                    let getMethodPropertyInfo = targetProperty.GetGetMethod()
+                    emitter.CallVirtual(getMethodPropertyInfo) |> ignore
+                    targetProperty.PropertyType
 
-            let rec generateMulticallBody (multicall: Ast.MultiCall, allProperties: PropertyInfo[], objType: Type) =
-                // TODO: refactoring required here. allProperties and objType can be removed.
                 match multicall with
-                | Ast.ThisPropertyCall (ident) ->
-                    let (Ast.Identifier propertyName) = ident
-                    let property = allProperties |> Seq.find(fun x -> x.Name = propertyName)
-                    let getMethod = property.GetGetMethod()
-                    emitter.LoadArgument(uint16 0)
-                    emitter.CallVirtual(getMethod) |> ignore
-                    property.PropertyType
+                | Ast.ThisPropertyCall (identifier) ->
+                    let (Ast.Identifier targetPropertyName) = identifier
+                    emitter.LoadArgument(uint16 0) |> ignore    // Emit: load this reference onto stack
+                    createPropertyCall(targetTypeProperties, targetPropertyName)
                 | Ast.ObjectPropertyCall (prevCall, ident) ->
-                    let subPropertyType = generateMulticallBody(prevCall, allProperties, objType)
-                    let (Ast.Identifier propertyName) = ident
-                    let property = subPropertyType.GetProperties() |> Seq.find(fun x -> x.Name = propertyName)
-                    let getMethod = property.GetGetMethod()
-                    emitter.CallVirtual(getMethod) |> ignore
-                    property.PropertyType
+                    let subPropertyType = generateMulticallBody(prevCall, thisType)
+                    let (Ast.Identifier targetPropertyName) = ident
+                    createPropertyCall(subPropertyType.GetProperties(), targetPropertyName)
                 | _ -> failwith "Unknown Multicall identifier"
 
-            let rec generateMethodBodyInt (program: Ast.Program, allProperties: PropertyInfo[]) =
+            let rec generateMethodBodyInt (program: Ast.Program) =
                 match program with
                 | Ast.BinaryExpression (leftExpr, operator, rightExpr) ->
                     let loadExpressionResultOnStack () =
-                        generateMethodBodyInt(leftExpr, allProperties)
-                        generateMethodBodyInt(rightExpr, allProperties)
-                        ()
+                        generateMethodBodyInt leftExpr
+                        generateMethodBodyInt rightExpr
 
                     match operator with
                     | Ast.Add ->
@@ -83,25 +85,23 @@ type public ClassBuilder(targetType:Type) =
                     | Ast.Int32Literal (v) ->
                         emitter.LoadConstant(v) |> ignore
                     | _ -> failwith "Unknown literal"
-                | Ast.MultiCallExpression (literal) ->
-                    generateMulticallBody(literal, allProperties, objType) |> ignore
+                | Ast.MultiCallExpression (multicall) ->
+                    generateMulticallBody(multicall, objType) |> ignore
                 | _ -> failwith "Unknown expression"
 
 
-            generateMethodBodyInt((AstBuilder.build expression), allProperties)
-            emitter.Return()
+            generateMethodBodyInt(AstBuilder.build expression)
+            emitter.Return() |> ignore
             emitter.CreateMethod()
 
-        let createProperty (property:PropertyInfo, expression, allProperties) =
+        let buildProperty (property:PropertyInfo, expression) =
             let propertyBuilder = typeBuilder.DefineProperty(property.Name, PropertyAttributes.HasDefault, property.PropertyType, null);
                 
-            let getMethodBuilder = createGetPropertyMethodBuilder(property.Name, property.PropertyType, expression, allProperties)
-            propertyBuilder.SetGetMethod(getMethodBuilder);
+            let getMethodBuilder = createGetPropertyMethodBuilder(property.Name, property.PropertyType, expression)
+            propertyBuilder.SetGetMethod(getMethodBuilder) |> ignore
 
-            ()
-        let allProperties = objType.GetProperties()
         for propertyExpr in propertyExpressions do
-            createProperty(propertyExpr.Property, propertyExpr.Expr, allProperties)
+            buildProperty(propertyExpr.Property, propertyExpr.Expr)
 
         typeBuilder.CreateType()
 
@@ -120,8 +120,7 @@ type public ClassBuilder<'T when 'T: null>() =
 
     member this.Setup<'TProperty> (property:System.Linq.Expressions.Expression<Func<'T, 'TProperty>>, expression: string):ClassBuilder<'T> = 
         let body = property.Body :?> System.Linq.Expressions.MemberExpression
-        base.Setup(body.Member.Name, expression)
-        this
+        base.Setup(body.Member.Name, expression) :?> ClassBuilder<'T>
 
     member this.BuildObject ():'T = 
         base.BuildObject() :?> 'T
