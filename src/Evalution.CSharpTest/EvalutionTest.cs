36dd3252c96432f8e2878b781a86167891407d72
Dan Pristupov <danil@danil.cz>
2015-01-15 20:11:27 +0100

    Adds GeneralTest_RuntimeProperties unit test

--- src/Evalution.CSharpTest/EvalutionTest.cs
+++ src/Evalution.CSharpTest/EvalutionTest.cs
@@ -24,6 +24,25 @@ namespace Evalution.CSharpTest
         }
 
         [Test]
+        public void GeneralTest_RuntimeProperties()
+        {
+            var classBuilder = new ClassBuilder(typeof(object))
+                .SetupRuntime("ValueWithExpression", typeof(int), "2+2*2")
+                .SetupRuntime("DependentValue2", typeof(int), "ValueWithExpression*2")
+                //                .Setup("AutoProperty", typeof (double))
+                ;
+            var target = (object)classBuilder.BuildObject();
+            var type = target.GetType();
+            var ValueWithExpressionValue = type.GetProperty("ValueWithExpression").GetValue(target, null);
+            var DependentValue2Value = type.GetProperty("DependentValue2").GetValue(target, null);
+            var aa = 23;
+            //            target.Value1 = 4;
+            //            Assert.AreEqual(6, target.ValueWithExpression); // "2+2*2"
+            //            Assert.AreEqual(8, target.DependentValue1);     // "Value1*2"
+            //            Assert.AreEqual(16, target.DependentValue2);    // "DependentValue1*2"
+        }
+
+        [Test]
         public void GeneralTest_Generic()
         {
             var classBuilder = new ClassBuilder<ClassInt32>()
